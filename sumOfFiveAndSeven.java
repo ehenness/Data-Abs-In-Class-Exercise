79b6628a1481cb9402f24347a68e7e4b5132a3a6
ehenness <159827456+ehenness@users.noreply.github.com>
2024-09-05 11:19:34 -0500

    Update sumOfFiveAndSeven.java

--- sumOfFiveAndSeven.java
+++ sumOfFiveAndSeven.java
@@ -15,7 +15,7 @@ public class sumOfFiveAndSeven {
     private static boolean repeat(int[] nums) {
         for (int i = 0; i < nums.length; i++){
             for(int j = 0; j < nums.length-1; j++){
-                if (i == j){
+                if (nums[i] == nums[j]){
                     return true;
                 }
             }
